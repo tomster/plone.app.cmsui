d3b054e293715500237b64f6545aa00c883f2070
Tom Lazar <tom@tomster.org>
2011-11-08 11:35:43 -0800

    depend on p.a.toolbar

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(name='plone.app.cmsui',
           'Products.CMFPlone',
           'plone.namedfile[blobs]',
           'plone.formwidget.namedfile',
+          'plone.app.toolbar',
           # XXX: Unclear why this isn't being pulled into a plain Plone 4.1 site
           'zope.browserresource',
       ],
